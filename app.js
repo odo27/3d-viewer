2c559ac3f03cc2009006120224f98cf8b81bafbe
odo27 <84677292+odo27@users.noreply.github.com>
2022-08-05 01:15:26 +0900

    final commit of ict project
    It's a overnight project in ICT smart device a thon. I made server and routing for pyqt requests.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+process.umask(0);
 var express = require('express');
 var multer = require('multer');
 var bodyParser = require('body-parser');
@@ -8,7 +9,7 @@ const fs = require('fs');
 
 const mysql = require('mysql');
 const conn = {
-  host: 'localhost',
+  host: '34.64.245.91',
   port: '3306',
   user: 'root',
   password: 'qwerasdf12',
@@ -36,6 +37,17 @@ var upload = multer({
   storage: storage
 });
 
+var upload2 = multer({
+  storage: multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, __dirname+'/public/userDrive/ict_test_id');
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
+    }
+  })
+});
+
 app.use(express.static('public'))
 
 app.listen(8080, function() {
@@ -52,6 +64,10 @@ app.post('/test/save', upload.array('IMG_FILE'), function(req, res) {
   console.log(req.files.length);
 
   for (let i=0; i<req.files.length; i++) {
+	  var oldname = __dirname + '/public/tempDir/' + req.files[i].originalname;
+	  var newname = __dirname + '/public/userDrive/' + req.body.path + '/' + req.files[i].originalname;
+	  console.log(oldname);
+	  console.log(newname);
     fs.rename(__dirname + '/public/tempDir/' + req.files[i].originalname, __dirname + '/public/userDrive/' + req.body.path + '/' + req.files[i].originalname, function (err) {
       if (err) {
         return console.error(err);
@@ -87,7 +103,7 @@ app.post('/process/create', function(req, res) {
     } else {
       var createUserInfo = "INSERT INTO ict.user VALUES ('" + req.body.name + "', '" + req.body.email + "', '" + req.body.id + "', '" + req.body.password + "');";
       connection.query(createUserInfo, function(err, results, fields) {
-        fs.mkdir(path.join(__dirname, 'public/userDrive', req.body.id), (err) => {
+        fs.mkdir(path.join(__dirname, 'public/userDrive', req.body.id), 0777, (err) => {
           if (err) {
             return console.log(err);
           }
@@ -117,3 +133,41 @@ app.post('/process/login', function(req, res) {
     };
   });
 })
+
+app.get('/download/:id/:file', function(req, res){
+  console.log(req.query);
+  var file = __dirname + '/public/userDrive/' + req.params.id + '/' + req.params.file;
+  if (req.params.file != undefined || req.params.id != undefined) {
+	  res.download(file);
+  }
+});
+
+app.get('/download/:file', function(req, res){
+  var file = __dirname + '/' + req.params.file;
+  res.download(file);
+});
+
+app.get('/myfiles/:id', function(req, res){
+  console.log(req.params.id);
+  var directoryPath = __dirname + '/public/userDrive/' + req.params.id;
+  fs.readdir(directoryPath, function(err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
+    }
+    console.log(files);
+    res.send(files);
+  });
+});
+
+app.post('/upload', upload2.single('file'), function(req, res) {
+  console.log('here');
+  res.sendStatus(200);
+});
+
+app.get('/vision', function(req, res) {
+  res.sendFile(__dirname + '/public/vision.html');
+});
+
+app.get('/reconstruction', function(req, res) {
+  res.sendFile(__dirname + '/public/reconstruction.html');
+});
